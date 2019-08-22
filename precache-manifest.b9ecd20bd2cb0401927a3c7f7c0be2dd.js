c8a5b3e3f152ed86ef8d364450e63d8fe6e3f2f0
Burn Esquivel <bernard.esquivel@mnltechnology.com>
2019-08-22 18:29:06 +0800

    boilerplate

---
+++ precache-manifest.b9ecd20bd2cb0401927a3c7f7c0be2dd.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb0d1515abb869eb13f0207432cf8791",
+    "url": "assets/anims/symbol/cof_allSymbols_final.atlas"
+  },
+  {
+    "revision": "ca9da2b2b3a36b77f14bda54c7971620",
+    "url": "assets/anims/symbol/cof_allSymbols_final.json"
+  },
+  {
+    "revision": "e1e3b3df6ba52693142b41603c66d467",
+    "url": "assets/anims/symbol/cof_allSymbols_final.png"
+  },
+  {
+    "revision": "573ed1ebc07927ece4c15af50e42621c",
+    "url": "assets/icons/icon-128x128.png"
+  },
+  {
+    "revision": "4d0cf2a482f47585629895849f8cbabb",
+    "url": "assets/icons/icon-144x144.png"
+  },
+  {
+    "revision": "ddeef34939ef17bb69624205140857c2",
+    "url": "assets/icons/icon-152x152.png"
+  },
+  {
+    "revision": "ed53cea5f0ba2c6369ebf2e08de0cfd4",
+    "url": "assets/icons/icon-192x192.png"
+  },
+  {
+    "revision": "6d41d9911c4281e7427927af5a093dab",
+    "url": "assets/icons/icon-384x384.png"
+  },
+  {
+    "revision": "ada841d3ed8f29ac831468b43d66b9c4",
+    "url": "assets/icons/icon-512x512.png"
+  },
+  {
+    "revision": "c012c516689e3e9debb06b9749c8fcaf",
+    "url": "assets/icons/icon-72x72.png"
+  },
+  {
+    "revision": "8c0d8df68f5007840dc205958ea5be0a",
+    "url": "assets/icons/icon-96x96.png"
+  },
+  {
+    "revision": "7476b3901cd882488fddd28653b3fc15",
+    "url": "assets/images/logo.png"
+  },
+  {
+    "revision": "78ac40b7984978acf7551e319ea5cd73",
+    "url": "assets/img/hero.png"
+  },
+  {
+    "revision": "a5d0e1965006de796e8c9badacdb22d5",
+    "url": "assets/img/symbols.json"
+  },
+  {
+    "revision": "7b90ce34c191b9a2cd706bc17d965464",
+    "url": "assets/img/symbols.png"
+  },
+  {
+    "revision": "e960fdbf5a1fc2501f2790000cb4f87a",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "4049c77c7ea16bc213f8e65c0725526f",
+    "url": "index.html"
+  },
+  {
+    "revision": "a9dde23b5a30d7e2767e",
+    "url": "main.bundle.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,35 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "precache-manifest.b9ecd20bd2cb0401927a3c7f7c0be2dd.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/\.(?:png|jpg|jpeg|svg)$/, new workbox.strategies.StaleWhileRevalidate({ "cacheName":"images", plugins: [new workbox.expiration.Plugin({ maxEntries: 20, purgeOnQuotaError: false })] }), 'GET');
+workbox.routing.registerRoute(/^https:\/\/rslots\.gp2play\.com\/captainrabbit\/?fun=1&op=w88&log=1&lang=en/, new workbox.strategies.StaleWhileRevalidate({ "cacheName":"captainRabbit", plugins: [new workbox.cacheableResponse.Plugin({ statuses: [ 0, 200 ] })] }), 'GET');
